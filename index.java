8ea17fa59cfa4c58c9221679c0be67135de0928a
MunawerSanoos <SanoosMunawer1123@gmail.com>
2023-09-02 17:46:14 +0530

    developed user profile and report

--- index.java
+++ index.java
@@ -33,3 +33,27 @@ class JobSeekerInterface : public QWidget {
     // Code for job seeker interface here
 };
 
+class Appointment {
+public:
+    Appointment(Consultant c, JobSeeker js, TimeSlot ts);
+    // Other functions for scheduling, canceling, etc.
+private:
+    Consultant consultant;
+    JobSeeker jobSeeker;
+    TimeSlot timeSlot;
+    // Logic to prevent double booking
+};
+
+class User {
+    Profile profile; // Contains personal details, etc.
+    // Other user-related functions
+};
+
+
+class Reports {
+    // Functions to generate various reports
+};
+
+class Analytics {
+    // Functions for insights and data analysis
+};
