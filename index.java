60c3a48dd54c2c2ee31b85138f1b010a95600820
MunawerSanoos <SanoosMunawer1123@gmail.com>
2023-09-02 17:49:08 +0530

    developed role interface

--- index.java
+++ index.java
@@ -57,3 +57,148 @@ class Reports {
 class Analytics {
     // Functions for insights and data analysis
 };
+
+
+import java.util.HashMap;
+
+class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
+class UserDatabase {
+    private static HashMap<String, User> users = new HashMap<>();
+
+    public static void addUser(User user) {
+        users.put(user.getUsername(), user);
+    }
+
+    public static User getUser(String username) {
+        return users.get(username);
+    }
+}
+
+public class AuthenticationSystem {
+    public static boolean authenticate(String username, String password) {
+        User user = UserDatabase.getUser(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        UserDatabase.addUser(new User("john", "password123"));
+        UserDatabase.addUser(new User("susan", "securePass"));
+
+        String username = "john";
+        String password = "password123";
+
+        if (authenticate(username, password)) {
+            System.out.println("Authentication successful!");
+        } else {
+            System.out.println("Authentication failed!");
+        }
+    }
+}
+
+
+
+// Role interface
+interface Role {
+    void performRoleFunctionality();
+}
+
+// Concrete roles
+class Administrator implements Role {
+    public void performRoleFunctionality() {
+        System.out.println("Administrator performing role functionality.");
+    }
+}
+
+class Consultant implements Role {
+    public void performRoleFunctionality() {
+        System.out.println("Consultant performing role functionality.");
+    }
+}
+
+class JobSeeker implements Role {
+    public void performRoleFunctionality() {
+        System.out.println("Job Seeker performing role functionality.");
+    }
+}
+
+// Factory interface
+interface RoleFactory {
+    Role createRole();
+}
+
+// Concrete factories
+class AdministratorFactory implements RoleFactory {
+    public Role createRole() {
+        return new Administrator();
+    }
+}
+
+class ConsultantFactory implements RoleFactory {
+    public Role createRole() {
+        return new Consultant();
+    }
+}
+
+class JobSeekerFactory implements RoleFactory {
+    public Role createRole() {
+        return new JobSeeker();
+    }
+
+
+// Abstract Report interface
+interface Report {
+    void generateReport();
+}
+
+// Concrete report types
+class AppointmentStatisticsReport implements Report {
+    public void generateReport() {
+        System.out.println("Generating Appointment Statistics Report.");
+    }
+}
+
+class ConsultantUtilizationReport implements Report {
+    public void generateReport() {
+        System.out.println("Generating Consultant Utilization Report.");
+    }
+}
+
+// Abstract Report Factory
+interface ReportFactory {
+    Report createReport();
+}
+
+// Concrete Report Factories
+class AppointmentStatisticsReportFactory implements ReportFactory {
+    public Report createReport() {
+        return new AppointmentStatisticsReport();
+    }
+}
+
+class ConsultantUtilizationReportFactory implements ReportFactory {
+    public Report createReport() {
+        return new ConsultantUtilizationReport();
+    }
+}
+
+
